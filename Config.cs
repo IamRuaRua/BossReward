899db04e44a6e463247f4f852243cfbcdefa800f
RuaRua <1536441243@qq.com>
2022-08-23 20:00:17 +0800

    添加项目文件。

---
+++ Config.cs
@@ -0,0 +1,78 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using TShockAPI;
+
+namespace BossReward
+{
+    public class Config
+    {
+        //路径
+        public static string ConfigPath = $"{TShock.SavePath}/BossReward.json";
+        public int 史莱姆王;
+        public int 克苏鲁之眼;
+        public int 克苏鲁之脑;
+        public int 世界吞噬者;
+        public int 骷髅王;
+        public int 鹿角怪;
+
+        public int 肉山;
+        public int 史莱姆女王;
+        public int 双子魔眼;
+        public int 机械骷髅王;
+        public int 机械虫子;
+        public int 猪鲨;
+        public int 光之女皇;
+
+        public int 世纪之花;
+        public int 石巨人;
+
+        public int 邪恶教徒;
+        public int 星尘柱;
+        public int 日曜柱;
+        public int 星云柱;
+        public int 旋涡柱;
+        public int 月球领主;
+        public int 蜂后;
+        /// <summary>
+        /// 确认配置文件存在，不存在则创建并填入默认值
+        /// </summary>
+        public static void EnsureFile()
+        {
+            if (!File.Exists(ConfigPath))
+            {
+                File.WriteAllText(ConfigPath, JsonConvert.SerializeObject(new Config(160,200, 220, 240, 260, 220, 320, 360, 380, 420, 440, 420, 500, 500, 500, 500, 500, 500, 500,500,500,280)));
+         
+            }
+        }
+        public static Config ReadConfig()
+        {
+            //读取ConfigFile
+            return JsonConvert.DeserializeObject<Config>(File.ReadAllText(ConfigPath));
+        }
+        public Config(int num1,int num2, int num3, int num4, int num5, int num6, int num7, int num8, int num9, int num10, int num11, int num12, int num13, int num14, int num15, int num16, int num17, int num18, int num19, int num20, int num21, int num22)
+        {
+            史莱姆王 = num1;   //50
+            克苏鲁之眼 = num2; //4
+            克苏鲁之脑 = num3; //266
+            世界吞噬者 = num4; //13
+            骷髅王 = num5;     //35
+            鹿角怪 = num6;     //668
+            肉山 = num7;       //113
+            史莱姆女王 = num8; //657
+            双子魔眼 = num9;   //125//126
+            机械骷髅王 = num10;//127
+            机械虫子 = num11;  //134
+            猪鲨 = num12;      //370
+            光之女皇 = num13;  //636
+            世纪之花 = num14;  //262
+            石巨人 = num15;   //245
+            邪恶教徒 = num16;//440
+            星尘柱 = num17;//493
+            日曜柱 = num18;//517
+            星云柱 = num19;//507
+            旋涡柱 = num20;//422
+            月球领主 = num21;//398
+            蜂后 = num22; //222
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using Terraria;
+using TerrariaApi.Server;
+using TShockAPI; 
+using System.Threading;
+using System.Collections;
+using Terraria.Localization; 
+
+namespace BossReward
+{
+    [ApiVersion(2, 1)]
+    public class BossReward : TerrariaPlugin
+    {
+        Config configFile;
+        public override string Author => "Rua";
+        public override string Description => "多人联机boss击败奖励插件";
+        public override string Name => "BossReward";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version; public BossReward(Main game) : base(game) { }
+        public ArrayList OnlinePlayers = new ArrayList();
+  
+        public override void Initialize()
+        {   
+            ServerApi.Hooks.NpcKilled.Register(this, OnNpcKilled); 
+            ServerApi.Hooks.ServerJoin.Register(this, OnServerjoin);
+            ServerApi.Hooks.ServerLeave.Register(this, OnServerLeave);
+            Commands.ChatCommands.Add(new Command(permissions: "ResetLifeValue", cmd: this.Cmd,"rlv"));
+            Config.EnsureFile();//读取配置文件
+            configFile = Config.ReadConfig(); 
+
+        }
+
+        void OnNpcKilled(NpcKilledEventArgs args)
+        {
+            int BossID = args.npc.netID;
+            for (int i=0; i < OnlinePlayers.Count; i++)
+            {
+                var player = TSPlayer.FindByNameOrID((string)OnlinePlayers[i])[0];
+                if (player != null)
+                {
+                    FoundPlayer found = Util.GetPlayer(player, player.Name);
+                    if (found.valid)
+                    {
+                        int nowMaxHP = found.plr.TPlayer.statLifeMax;
+                        int maxHP = GetPlayerMaxHP(BossID);
+                        if (maxHP>nowMaxHP&&maxHP!=0)
+                        { 
+                            found.plr.TPlayer.statLifeMax = maxHP;
+                            NetMessage.SendData((int)PacketTypes.PlayerHp, -1, -1, NetworkText.Empty, found.plr.Index, 0f, 0f, 0f, 0);
+                            player.SendInfoMessage($"{found.Name} 的生命上限已修改为 {maxHP}");
+                        } 
+                    }
+                   
+                }
+            }
+        }
+        void OnServerjoin(JoinEventArgs args)
+        {
+            TSPlayer tSPlayer = TShock.Players[args.Who];
+            Group group = tSPlayer.Group;
+            string playergroup = group.Name;
+            string name = tSPlayer.Name; 
+            OnlinePlayers.Add(name);
+        } 
+        void OnServerLeave(LeaveEventArgs args)
+        {
+            TSPlayer tSPlayer = TShock.Players[args.Who];
+            Group group = tSPlayer.Group;
+            string playergroup = group.Name;
+            string name = tSPlayer.Name;
+            OnlinePlayers.Remove(name);
+            
+        } 
+       void Cmd(CommandArgs args)
+        {
+            var player = args.Player;
+            if (player == null) return;  
+            FoundPlayer found = Util.GetPlayer(args.Player,player.Name);
+            if (!found.valid)
+            { 
+                return;
+            }
+            int maxHP = 100;
+            found.plr.TPlayer.statLifeMax = maxHP;
+            NetMessage.SendData((int)PacketTypes.PlayerHp, -1, -1, NetworkText.Empty, found.plr.Index, 0f, 0f, 0f, 0);
+             args.Player.SendSuccessMessage($"{found.Name} 的生命上限已修改为 {maxHP}");
+             
+         }
+        int GetPlayerMaxHP(int BossID)
+        {
+            switch (BossID)
+            {
+                case 4:
+                    return configFile.克苏鲁之眼;
+                case 50:
+                    return configFile.史莱姆王;
+                case 668:
+                    return configFile.鹿角怪;
+                case 13:
+                    return configFile.世界吞噬者;
+                case 266:
+                    return configFile.克苏鲁之脑;
+                case 35:
+                    return configFile.骷髅王;   
+                case 134:
+                    return configFile.机械虫子;
+                case 657:
+                    return configFile.史莱姆女王;
+                case 398:
+                    return configFile.月球领主;
+                case 262:
+                    return configFile.世纪之花;
+                case 222:
+                    return configFile.蜂后;
+                case 127:
+                    return configFile.机械骷髅王;
+                case 370:
+                    return configFile.猪鲨;
+                case 636:
+                    return configFile.光之女皇;
+                case 245:
+                    return configFile.石巨人;
+                case 507:
+                    return configFile.星云柱;
+                case 113:
+                    return configFile.肉山;
+                case 517:
+                    return configFile.日曜柱;
+                case 493:
+                    return configFile.星尘柱;
+                case 422:
+                    return configFile.旋涡柱;
+                case 440:
+                    return configFile.邪恶教徒;
+                default:
+                    return 0;
+            }
+        }
+     }
+         
+}
+ 
